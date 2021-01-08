0b82503ddec240ecd3be9270e1eca1c8c502b714
hechuan <359702798@qq.com>
2021-01-08 17:28:27 +0800

    add代码

--- Springbootstudy/com.atguigu.springboot_web_01/src/main/java/com/atguigu/boot/controller/TestController.java
+++ Springbootstudy/com.atguigu.springboot_web_01/src/main/java/com/atguigu/boot/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.atguigu.boot.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author: 何川
@@ -10,4 +10,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @GetMapping("/user")
+    //@RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getUser(){
+        return "get所有用户";
+    }
+
+    @PostMapping("/user")
+    //@RequestMapping(value = "/user",method = RequestMethod.POST)
+    public String addUser(){
+
+        return "post了用户";
+    }
+
+    @PutMapping("/user")
+    //@RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String putUser(){
+        return "put用户成功";
+    }
+
+    @DeleteMapping("/user")
+    //@RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser(){
+          return "delete用户成功";
+    }
 }
